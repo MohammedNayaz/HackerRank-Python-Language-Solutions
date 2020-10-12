b5c3a079b68649efbb15df346b4311688dfd59af
Mohammed Nayaz <64147100+MohammedNayaz@users.noreply.github.com>
2020-10-12 10:02:22 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,5 @@
+#Loops
+if __name__ == '__main__':
+    n = int(input())
+    for i in range (0,n):
+        print (i*i)
---
+++ Python if-Else.py
@@ -0,0 +1,21 @@
+#Python if-Else
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if (n%2==1 or n>=6 and n<=20):
+        print('Weird')
+    elif (n%2==1 or n>=2 and n<=5):
+        print('Not Weird')
+    else:
+        print('Not Weird')
