ef6c7d06d553ad244026bedf7ae71db28b5f30e4
Mohammed Nayaz <64147100+MohammedNayaz@users.noreply.github.com>
2020-10-19 11:12:54 +0530

    Add files via upload

---
+++ String Split and Join.py
@@ -0,0 +1,11 @@
+#String Split and Join
+def split_and_join(line):
+    # write your code here
+    x=line.split(" ") 
+    x='-'.join(x)    
+    return x
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
---
+++ swapcase.py
@@ -0,0 +1,8 @@
+#swapcase
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
