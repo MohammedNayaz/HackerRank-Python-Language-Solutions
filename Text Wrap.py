0b39a497dfda484b98bc94e78cafb35f2db1c8e2
Mohammed Nayaz <64147100+MohammedNayaz@users.noreply.github.com>
2020-10-24 17:49:21 +0530

    Add files via upload

---
+++ Text Wrap.py
@@ -0,0 +1,11 @@
+#Text Wrap
+import textwrap
+
+def wrap(string, max_width):
+    string.split()
+    return textwrap.fill(string,max_width)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
